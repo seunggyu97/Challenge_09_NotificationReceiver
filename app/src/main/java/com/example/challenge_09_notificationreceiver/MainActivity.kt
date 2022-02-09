1d5600db5b696586bd27c485d7d24c25af8a1d7d
seunggyu97 <seunggyu97@gmail.com>
2022-02-10 03:58:30 +0900

    First Commit

---
+++ app/src/main/java/com/example/challenge_09_notificationreceiver/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.challenge_09_notificationreceiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
