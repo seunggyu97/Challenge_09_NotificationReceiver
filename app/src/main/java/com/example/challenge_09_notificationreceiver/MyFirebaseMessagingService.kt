2996485cb79ce903476ff0a8b575e5c7c45d0fb1
seunggyu97 <seunggyu97@gmail.com>
2022-02-16 04:07:44 +0900

    완성

--- app/src/main/java/com/example/challenge_09_notificationreceiver/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/challenge_09_notificationreceiver/MyFirebaseMessagingService.kt
@@ -3,7 +3,10 @@ package com.example.challenge_09_notificationreceiver
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.app.NotificationBuilderWithBuilderAccessor
@@ -56,11 +59,19 @@ class MyFirebaseMessagingService: FirebaseMessagingService(){
                                    title: String?,
                                    message: String?
     ): Notification {
+        val intent = Intent(this, MainActivity::class.java).apply{
+            putExtra("notificationType","${type.title}타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivity(this,type.id,intent,FLAG_UPDATE_CURRENT)
+
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_notifications_24)
             .setContentTitle(title)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         when(type) {
             NotificationType.NORMAL -> Unit
